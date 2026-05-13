a8e112f7df8ba4494573886409128d4d9d8cc496
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2026-05-13 09:03:36 +0200

    feat:added endpoint auto complete for internal endpoints to be suggested

--- shesha-reactjs/src/designer-components/inputComponent/wrappers/endpointsAutocomplete.tsx
+++ shesha-reactjs/src/designer-components/inputComponent/wrappers/endpointsAutocomplete.tsx
@@ -1,15 +1,23 @@
 import { IEndpointsAutocompleteSettingsInputProps } from '@/designer-components/settingsInput/interfaces';
 import React, { FC } from 'react';
 import { EndpointsAutocomplete } from '@/components';
+import { useShaFormInstance } from '@/providers';
+import { evaluateValueAsString } from '@/providers/form/utils';
 
 export const EndpointsAutocompleteWrapper: FC<IEndpointsAutocompleteSettingsInputProps> = (props) => {
   const { value, onChange, size, httpVerb } = props;
+  const { formData } = useShaFormInstance();
+
+  const resolvedHttpVerb = httpVerb
+    ? evaluateValueAsString(httpVerb, { data: formData }) ?? httpVerb
+    : httpVerb;
+
   return (
     <EndpointsAutocomplete
       {...props} // TODO: check spread
       // mode={props.mode}
       size={size}
-      httpVerb={httpVerb}
+      httpVerb={resolvedHttpVerb}
       value={value}
       onChange={onChange}
     />
